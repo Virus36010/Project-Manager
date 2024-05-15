b4e34206267b29a911258dbd4e344379b53ef35d
Virus <146997559+Virus36010@users.noreply.github.com>
2024-05-15 18:03:21 +0300

    Add files via upload

---
+++ project-manager-main/public/script.js
@@ -0,0 +1,77 @@
+async function CreateProject(projectName) {
+    const response = await fetch(`/project/${projectId}/CrProject`)
+}
+
+let btnFind = document.getElementById("btnFind")
+let idInpt = document.getElementById("idInpt")
+btnFind.addEventListener('click', ()=>{
+    loadProject(idInpt.value)
+})
+
+let btnCrPr = document.getElementById("btnCrPr")
+let nameInp = document.getElementById("namePr")
+btnCrPr.addEventListener('click', async ()=>{
+    await fetch(`/createPr/${nameInp.value}`)
+})
+
+async function loadProject(projectId) {
+  const response = await fetch(`/project/${projectId}`);
+  const project = await response.json();
+
+  const app = document.getElementById('app');
+  app.innerHTML = `<h1>${project.name}</h1>`;
+
+  const taskForm = document.createElement('form');
+  taskForm.innerHTML = `
+      <input type="text" id="taskDescription" placeholder="Новая задача" required>
+      <button type="submit">Add Task</button>
+  `;
+  taskForm.onsubmit = (e) => {
+      e.preventDefault();
+      addTask(projectId);
+  };
+  app.appendChild(taskForm);
+
+  project.tasks.forEach(task => {
+      const taskElement = document.createElement('div');
+      taskElement.className = 'task' + (task.completed ? ' completed' : '');
+      taskElement.innerHTML = `
+          <span>${task.description}</span>
+          <div>
+              <button onclick="deleteTask('${projectId}', '${task.id}')">Удалить</button>
+              <button onclick="toggleTask('${projectId}', '${task.id}', ${!task.completed})">${task.completed ? 'Unmark' : 'Отметить выполненым'}</button>
+          </div>
+      `;
+      app.appendChild(taskElement);
+  });
+}
+
+async function toggleTask(projectId, taskId, completed) {
+  await fetch(`/project/${projectId}/task`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ taskId, completed })
+  });
+  loadProject(projectId);
+}
+
+async function addTask(projectId) {
+  const taskDescription = document.getElementById('taskDescription').value;
+  await fetch(`/project/${projectId}/tasks`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ description: taskDescription })
+  });
+  loadProject(projectId);
+}
+
+async function deleteTask(projectId, taskId) {
+  await fetch(`/project/${projectId}/task/${taskId}`, {
+      method: 'DELETE'
+  });
+  loadProject(projectId);
+}
